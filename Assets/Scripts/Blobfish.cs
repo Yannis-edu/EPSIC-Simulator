3d25cfdad59e4e16f66ee21df21af7ae1a85b6ac
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2020-03-20 13:09:26 +0100

    Add labo with blobfish

---
+++ Assets/Scripts/Blobfish.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Blobfish : MonoBehaviour
+{
+    public float power;
+    private float timeToChangeDirection;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        timeToChangeDirection -= Time.fixedDeltaTime;
+        if (timeToChangeDirection <= 0)
+        {
+            timeToChangeDirection = Random.Range(0.5f, 2);
+            transform.Rotate(0, 0, Random.Range(0, 360));
+            rb.velocity = transform.up * power;
+        }
+    }
+}
