1818d14912d06ea68c517f3c32b333a3397b8464
Fabrice <47972290+80fabou80@users.noreply.github.com>
2020-03-24 12:16:55 +0100

    Dean's room
    Horizontal movement block if seated.

--- Assets/Scripts/chairDoyen.cs
+++ Assets/Scripts/chairDoyen.cs
@@ -19,7 +19,7 @@ public class chairDoyen : MonoBehaviour
 
     void Update()
     {
-    
+        
     }
 
         private void OnTriggerStay2D(Collider2D collision)
@@ -30,28 +30,29 @@ public class chairDoyen : MonoBehaviour
             if (SimpleInput.GetButtonDown("Vertical") && SimpleInput.GetAxis("Vertical") < 0)
             {
                 collision.gameObject.GetComponent<Player>().animator.SetBool("isAssis", true);
+                
                 ////////////////////////////////////////
-                //*** Déplacement horizontale stop ***//
-                bool mouvementH = SimpleInput.GetButtonDown("Horizontal");
-                mouvementH = false;
+                //*** Déplacement horizontale stop ***//                             
+                StaticClass.disableInput = true;
 
                 //*** positionnnement du player assis sur le sprite de la chaise ***//
                 //*** position de la chaise du doyen, x = -83 -- y = 145.5 ***//
-                chair1 = GameObject.Find("decor-chair"); //On cherche la position de la chaise 1
+                chair1 = GameObject.Find("Chair");
                 chair2 = GameObject.Find("player-chair");
 
-                Vector3 position = new Vector3(0, 0, 0);
+                Vector3 position = new Vector3(-83, 145, 0);
 
                 position.x = chair1.transform.position.x;
                 position.y = chair1.transform.position.y;
                 position.z = chair1.transform.position.z;
 
-                chair2.transform.position = position;  //La chaise2 prend la position de la chaise1
+                chair2.transform.position = position;
             }
 
             if (SimpleInput.GetButtonDown("Vertical") && SimpleInput.GetAxis("Vertical") > 0)
             {
                 collision.gameObject.GetComponent<Player>().animator.SetBool("isAssis", false);
+                StaticClass.disableInput = false;
             }
         }
     }
